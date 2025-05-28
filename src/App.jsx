d571816c581a37154af506655bbb4530ba08b2ef
Spherenex <spherenexdeveloper@gmail.com>
2025-05-28 11:10:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Vibration from '../src/components/vibration'
+import './App.css'
+import LineFallDetectionDashboard from './components/Linefall'
+import SingleAxis from './components/SingleAxis'
+import TransformerPrediction from './components/TranformerPrediction'
+import TransformerService from './components/TransformerService'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <Vibration/> */}
+      {/* <LineFallDetectionDashboard /> */}
+      {/* <SingleAxis/> */}
+      <TransformerPrediction />
+      {/* <TransformerService /> */}
+    </>
+  )
+}
+
+export default App
